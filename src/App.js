1af93d45d71c1df8be4a79ef4015595ff0d1614c
numan-ahmad <na56439@gami.com>
2021-06-09 23:52:22 +0500

    changing and add some functionlity

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Reminder from "./add_reminder";
+
+// const padTime = time => {
+//   return `${time}`;
+// };
+
+// const format = time => {
+//   const seconds = time % 60;
+
+//   //Return combined values as string in format mm:ss
+//   return `${padTime(seconds)}`;
+// };
 
 
 
 const App = ()=>{
+
+  const [seconds, setSecond] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(()=>{
+    console.log('first render', seconds);
+    if(isRunning){
+      const id= window.setInterval(()=>{
+        console.log('tick', seconds);
+        setSecond(seconds => seconds+1);
+      },1000);
+      return () => window.clearInterval(id);
+    }
+  }, [isRunning]);
+
+
+  // const [counter, setCounter] = useState(30);
+  // useEffect(() => {
+  //   let timer;
+  //   if (counter > 0) {
+  //     timer = setTimeout(() => setCounter(c => c - 1), 1000);
+  //   }
+
+  //   return () => {
+  //     if (timer) {
+  //       clearTimeout(timer);
+  //     }
+  //   };
+  // }, [counter]);
+
+
   const [item,setitem]=useState("");
   const [itemss, setitems]=useState([]);
 
@@ -14,7 +56,7 @@ const App = ()=>{
  };
  const listitem = () =>{
   setitems((olditems)=>{
-    return [...olditems,item]
+    return [...olditems,item];
   });
   setitem("");
 };
@@ -26,7 +68,10 @@ const deleteitem = (id) =>{
     });
   });
 };
-
+const put_false= () => {
+  setIsRunning(true);
+  listitem();
+}
   return(
 
 <>
@@ -44,11 +89,24 @@ const deleteitem = (id) =>{
          />;
        })}
      </ol></div>
+
   <div className="btn_input">
   <input type="text" placeholder="Add a Reminder" value={item} onChange={eventitem} />
-        <button onClick={listitem}>Click On</button>
-  </div>
 
+        <button onClick={put_false}>Click On</button>
+  </div>
+  <div className="App">
+  <div className='time'>
+          {`Count Down : ${seconds}`}
+        </div>
+        
+
+      </div>
+
+  {/* <div className="App">
+        {counter === 0 ? "Time up" : <div>Countdown: {format(counter)}</div>}
+      </div> */}
+      
 </div>
 
 </>
